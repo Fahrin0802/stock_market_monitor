d1829d317e523fefcf6612030c7892d4aa26c187
Rahul <rtabraha@ualberta.ca>
2024-02-12 11:50:12 -0700

    Add CLI, add gitignore

--- stock_market_monitor/src/main.rs
+++ stock_market_monitor/src/main.rs
@@ -1,67 +1,97 @@
+use time::{Duration, OffsetDateTime};
 use yahoo_finance_api as yahoo;
 use yahoo_finance_api::Quote;
-use time::{OffsetDateTime, Duration};
 
+use plotters::drawing::IntoDrawingArea;
 use plotters::prelude::*;
 use plotters::style::RGBColor;
-use plotters::drawing::IntoDrawingArea;
 
-use chrono::{NaiveDate, TimeZone, Datelike, Utc};
+use chrono::{Datelike, NaiveDate, TimeZone, Utc};
 
+use clap::Parser;
 
-fn get_stock_prices(stock_name: &str, end_date: OffsetDateTime, start_date: OffsetDateTime) -> Vec<Quote> {
+fn get_stock_prices(
+    stock_name: &str,
+    end_date: OffsetDateTime,
+    start_date: OffsetDateTime,
+) -> Vec<Quote> {
     let provider = yahoo::YahooConnector::new();
-    
+
     // returns historic quotes with daily interval
-    let resp = tokio_test::block_on(provider.get_quote_history(stock_name, start_date, end_date)).unwrap();
+    let resp =
+        tokio_test::block_on(provider.get_quote_history(stock_name, start_date, end_date)).unwrap();
     return resp.quotes().unwrap();
 }
 
-
-fn plot_prices(min_price: f64, max_price: f64, min_date: NaiveDate, max_date: NaiveDate, series: Vec<(NaiveDate, f64)>, stock_name: &str) -> Result<(), Box<dyn std::error::Error>> {
+fn plot_prices(
+    min_price: f64,
+    max_price: f64,
+    min_date: NaiveDate,
+    max_date: NaiveDate,
+    series: Vec<(NaiveDate, f64)>,
+    stock_name: &str,
+) -> Result<(), Box<dyn std::error::Error>> {
     // Create a drawing area
     let root = BitMapBackend::new("stock_prices.png", (800, 600)).into_drawing_area();
     root.fill(&RGBColor(255, 255, 255))?;
 
     // Configure a line chart
     let mut chart = ChartBuilder::on(&root)
-        .caption(stock_name.to_owned() + " Stock Prices", ("Arial", 30).into_font())
+        .caption(
+            stock_name.to_owned() + " Stock Prices",
+            ("Arial", 30).into_font(),
+        )
         .x_label_area_size(40)
         .y_label_area_size(40)
         .build_cartesian_2d(min_date..max_date, min_price..max_price)?;
 
     // Draw the line series
     chart.configure_mesh().draw()?;
-    chart
-        .draw_series(LineSeries::new(
-            series.iter().map(|(x, y)| (*x, *y)),
-            &RGBColor(255, 0, 0),
-        ))?;
+    chart.draw_series(LineSeries::new(
+        series.iter().map(|(x, y)| (*x, *y)),
+        &RGBColor(255, 0, 0),
+    ))?;
     Ok(())
 }
+/// Generate plots from inputted stock names
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Name of the stock ticker, ex. AAPL
+    stock_name: String,
+}
 
-
-fn main()  {
-    let stock_name = "AAPL";
+fn main() {
+    let args = Args::parse();
+    let stock_name: &str = &args.stock_name;
 
     // Get today's date and six months prior date
     let today = OffsetDateTime::now_utc();
     let six_months_ago = today - Duration::days(30 * 6);
 
     let quotes = get_stock_prices(stock_name, today, six_months_ago);
-    
 
     let mut series = Vec::new();
 
-    let min_date: NaiveDate = NaiveDate::from_ymd(six_months_ago.year(), six_months_ago.month() as u32, six_months_ago.day().into());
-    let max_date: NaiveDate = NaiveDate::from_ymd(today.year(), today.month() as u32, today.day().into());
+    // TODO remove deprecated functions
+    let min_date: NaiveDate = NaiveDate::from_ymd(
+        six_months_ago.year(),
+        six_months_ago.month() as u32,
+        six_months_ago.day().into(),
+    );
+    let max_date: NaiveDate =
+        NaiveDate::from_ymd(today.year(), today.month() as u32, today.day().into());
     let mut min_price: f64 = 10000.0;
     let mut max_price: f64 = 0.0;
 
-    for item in quotes{
+    for item in quotes {
         let datetime_utc = Utc.timestamp(item.timestamp as i64, 0);
-        let item_date: NaiveDate = NaiveDate::from_ymd(datetime_utc.year(), datetime_utc.month() as u32, datetime_utc.day().into());
-        
+        let item_date: NaiveDate = NaiveDate::from_ymd(
+            datetime_utc.year(),
+            datetime_utc.month() as u32,
+            datetime_utc.day().into(),
+        );
+
         let x: (NaiveDate, f64) = (item_date, item.close);
         series.push(x);
 
@@ -76,12 +106,12 @@ fn main()  {
     let _ = plot_prices(min_price, max_price, min_date, max_date, series, stock_name);
 }
 
-// Quote { 
-//     timestamp: 1577975400, 
-//     open: 74.05, 
-//     high: 75.15, 
+// Quote {
+//     timestamp: 1577975400,
+//     open: 74.05,
+//     high: 75.15,
 //     low: 73.79,
-//     volume: 135480400, 
-//     close: 75.08, 
+//     volume: 135480400,
+//     close: 75.08,
 //     adjclose: 73.05
 // }
--- stock_market_monitor/target/debug/stock_market_monitor.d
+++ stock_market_monitor/target/debug/stock_market_monitor.d
@@ -1,1 +1,1 @@
-D:\winter2024\ECE421\project\stock_market_monitor\stock_market_monitor\target\debug\stock_market_monitor.exe: D:\winter2024\ECE421\project\stock_market_monitor\stock_market_monitor\src\main.rs
+/Users/rahul/code/projects/stock_market_monitor/stock_market_monitor/target/debug/stock_market_monitor: /Users/rahul/code/projects/stock_market_monitor/stock_market_monitor/src/main.rs
