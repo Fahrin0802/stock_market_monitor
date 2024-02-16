d855fdc9bd2d5addb9caade8803eae03a4310763
Aditya Goyal <agoyal7000@gmail.com>
2024-02-15 20:37:02 -0700

    Fixes git ignore

--- stock_market_monitor/target/debug/stock_market_monitor.d
+++
@@ -1,1 +0,0 @@
-/Users/rahul/code/projects/stock_market_monitor/stock_market_monitor/target/debug/stock_market_monitor: /Users/rahul/code/projects/stock_market_monitor/stock_market_monitor/src/main.rs
