333b92216677126f45e2d2d6ddffe468ad970497
Aditya Goyal <agoyal7000@gmail.com>
2024-02-15 20:04:25 -0700

    Added comments and removed depracated methods

--- stock_market_monitor/src/main.rs
+++ stock_market_monitor/src/main.rs
@@ -79,10 +79,6 @@ fn is_valid_stock(stock_name: &str, provider: &YahooConnector) -> bool {
     }
 }
 
-struct DatePricePair {
-    date: NaiveDate,
-    price: f64,
-}
 /// Generate plots from inputted stock names
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -96,6 +92,8 @@ fn main() {
     let stock_name: &str = &args.stock_name;
 
     let provider = yahoo::YahooConnector::new();
+
+    // Check if stock symbol is valid
     if !is_valid_stock(stock_name, &provider) {
         eprintln!("Error: The stock symbol {} is not valid.", stock_name);
         process::exit(1);
@@ -105,79 +103,87 @@ fn main() {
     let today = OffsetDateTime::now_utc();
     let six_months_ago = today - Duration::days(30 * 6);
 
+    // Get daily stock quotes from yahoo finance
     let quotes: Vec<Quote> = get_stock_prices(stock_name, today, six_months_ago, &provider);
 
-
-    // TODO remove deprecated functions
-    let min_date: NaiveDate = NaiveDate::from_ymd(
+    // Convert date format to Naive Date
+    let min_date: NaiveDate = match NaiveDate::from_ymd_opt(
         six_months_ago.year(),
         six_months_ago.month() as u32,
         six_months_ago.day().into(),
-    );
-    let max_date: NaiveDate = NaiveDate::from_ymd(
+    ) {
+        Some(date) => date,
+        None => {
+            panic!("Invalid date");
+        }
+    };
+    let max_date: NaiveDate = match NaiveDate::from_ymd_opt(
         today.year(), 
         today.month() as u32, 
-        today.day().into());
+        today.day().into()) {
+            Some(date) => date,
+            None => {
+                panic!("Invalid date");
+            }
+        };
 
     //This iterator is created to help derive the daily data, volatile days and min/max close
     let date_quote_pairs  = quotes.iter().map(|quote| {
-        let datetime_utc = Utc.timestamp(quote.timestamp as i64, 0);
-        let quote_date: NaiveDate = NaiveDate::from_ymd(
+        let datetime_utc = Utc.timestamp_opt(quote.timestamp as i64, 0).unwrap();
+        let quote_date: NaiveDate = match NaiveDate::from_ymd_opt(
             datetime_utc.year(),
             datetime_utc.month() as u32,
             datetime_utc.day().into(),
-        );
+        ) {
+            Some(date) => date,
+            None => {
+                panic!("Invalid date");
+            }
+        };
         (quote_date, quote)
     });
 
+    // Derive daily closing prices
     let series: Vec<(NaiveDate, f64)> = date_quote_pairs.clone().map(|(quote_date, quote)|{
         (quote_date, quote.close)
     }).collect();
 
+    // Derive volatility data
     let volatile_days: Vec<(NaiveDate, Quote)> = date_quote_pairs.clone().filter(|(_, quote)| {
         (quote.high - quote.low) / quote.close > 0.02
     }).map(|(quote_date, quote)|{
         (quote_date, quote.clone())
     }).collect();
 
+    // Derive max and min closing prices over 6 months
     let (min_quote_date, min_quote_price) = date_quote_pairs.clone().min_by(|(_, quote1), (_, quote2)| {
         quote1.close.partial_cmp(&quote2.close).unwrap()})
     .map(|(date, quote)| (date, quote.close))
     .unwrap();
-
     let (max_quote_date, max_quote_price) = date_quote_pairs.max_by(|(_, quote1), (_, quote2)| {
         quote1.close.partial_cmp(&quote2.close).unwrap()})
     .map(|(date, quote)| (date, quote.close))
     .unwrap();
-
-    let min_item = DatePricePair {
-        date: min_quote_date,
-        price: min_quote_price,
-    };
-    let max_item = DatePricePair {
-        date: max_quote_date,
-        price: max_quote_price,
-    };
-
     println!(
         "{} Stats:\nMax Closing Price: ${:.2} on {}\nMin Closing Price: ${:.2} on {}",
-        stock_name, max_item.price, max_item.date, min_item.price, min_item.date
+        stock_name, max_quote_price, max_quote_date, min_quote_price, min_quote_date
     );
+
+    // Create a plot with volatility data and one without the volatility data
     let _ = plot_prices(
         "volatile_stock_prices.png",
-        min_item.price,
-        max_item.price,
+        min_quote_price,
+        max_quote_price,
         min_date,
         max_date,
         series.clone(),
         volatile_days,
         stock_name,
     );
-
     let _ = plot_prices(
         "stock_prices.png",
-        min_item.price,
-        max_item.price,
+        min_quote_price,
+        max_quote_price,
         min_date,
         max_date,
         series,
@@ -185,8 +191,8 @@ fn main() {
         stock_name,
     );
 
+    // Serve the generated plots on local host
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
@@ -201,19 +207,20 @@ fn handle_connection(mut stream: TcpStream) -> std::io::Result<()> {
     let mut buffer = [0; 1024];
     stream.read(&mut buffer)?;
 
-    if buffer.starts_with(b"GET / HTTP/1.1\r\n") {
+    // Handle the recieved request
+    if buffer.starts_with(b"GET / HTTP/1.1\r\n") { // Serve the html file
         let status_line = "HTTP/1.1 200 OK";
         let contents = std::fs::read_to_string("src/plots.html").unwrap();
         let length = contents.len();
 
         stream.write_all(format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}").as_bytes())?;
         Ok(())
-    } else if buffer.starts_with(b"GET /stock_image.png HTTP/1.1\r\n") {
+    } else if buffer.starts_with(b"GET /stock_image.png HTTP/1.1\r\n") { // Serve the plot without volatility data
         write_file_to_stream("stock_prices.png", stream)
     
-    } else if buffer.starts_with(b"GET /volatile_image.png HTTP/1.1\r\n") {
+    } else if buffer.starts_with(b"GET /volatile_image.png HTTP/1.1\r\n") { // Serve the plot with volatility data
         write_file_to_stream("volatile_stock_prices.png", stream)
-    } else {
+    } else { // Invalid request case
         println!("Invalid Request");
         Ok(())
     }
